8bbea59bf2c17cbd49ecba56ef4473eb834aa967
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-04 10:27:29 +0600

    nav bar set up

---
+++ e-school/src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css'
+import Navbar from './Navbar/Navbar';
+
+const Home = () => {
+      return (
+            <div>
+                 <Navbar></Navbar> 
+            </div>
+      );
+};
+
+export default Home;
