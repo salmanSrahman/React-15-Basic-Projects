47be7d8320e77d499b39e01b654f9edf638888d6
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-05 18:57:38 +0600

    teacher icon set up

--- online-school/src/Components/Home/HomeTeachers/HomeTeacher/HomeTeacher.js
+++ online-school/src/Components/Home/HomeTeachers/HomeTeacher/HomeTeacher.js
@@ -1,9 +1,45 @@
 import React from "react";
+import "./HomeTeacher.css";
+import { Link } from "react-router-dom";
 
-const HomeTeacher = () => {
+const HomeTeacher = (props) => {
+  console.log(props.teacher);
+  const { img, title, name } = props.teacher;
   return (
-    <div>
-      <h1>Single Teacher</h1>
+    <div className="col-xl-3 col-md-6">
+      <div className="teacher-card border">
+        <div className="teacher-img">
+          <img src={img} alt="" className="img-fluid" />
+          <div className="teacher-overlay d-flex align-items-center">
+            <ul>
+              <li className="text-center">
+                <a href="#">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-pinterest-p"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="teacher-text p-3">
+          <h4 className="green-color">{name}</h4>
+          <h6>{title}</h6>
+        </div>
+      </div>
     </div>
   );
 };
--- online-school/src/Components/Home/HomeTeachers/HomeTeachers.js
+++ online-school/src/Components/Home/HomeTeachers/HomeTeachers.js
@@ -1,20 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./HomeTeachers.css";
+import HomeTeacher from "../HomeTeachers/HomeTeacher/HomeTeacher";
 
 const HomeTeachers = () => {
+  const [teachers, setTeachers] = useState([]);
+
   useEffect(() => {
-    fetch("./");
+    fetch("./someTeacher.JSON")
+      .then((res) => res.json())
+      .then((data) => setTeachers(data));
   });
 
   return (
-    <div>
+    <div className="someTeacher-part py-5">
       <div className="container">
         <div className="title mb-3">
           <h2 className="text-center">
-            All <span class="green-color">Courses</span>
+            Some <span class="green-color">Teachers</span>
           </h2>
         </div>
-        <div className="row g-3"></div>
+        <div className="row g-3">
+          {teachers.map((teacher) => (
+            <HomeTeacher teacher={teacher}></HomeTeacher>
+          ))}
+        </div>
       </div>
     </div>
   );
