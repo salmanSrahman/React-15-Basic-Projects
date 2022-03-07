f9050a255179b47e7f4691a9da189d3ba78f0c21
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-07 22:23:50 +0600

    footer social added

--- online-school/src/Components/Footer/Footer.js
+++ online-school/src/Components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer-part ">
       <div className="container">
-        <div className="row g-3">
+        <div className="row g-5">
           <div className="col-xl-3 col-md-6 text-light">
             <div className="footer-title pb-4">
               <h5 className="text-light text-uppercase">About Us</h5>
@@ -32,9 +33,29 @@ const Footer = () => {
               </a>
             </div>
           </div>
-          <div className="col-xl-3 col-md-6"></div>
-          <div className="col-xl-3 col-md-6"></div>
-          <div className="col-xl-3 col-md-6"></div>
+          <div className="col-xl-3 col-md-6">
+            <div className="footer-title pb-4">
+              <h5 className="text-light text-uppercase">QUICKLINKS</h5>
+            </div>
+            <div className="footer-second-text pb-3">
+              <Link to="/home">Home</Link>
+              <Link to="/about">About Us</Link>
+              <Link to="/allCourses">Courses</Link>
+              <Link to="/contact">Contact</Link>
+            </div>
+          </div>
+          <div className="col-xl-3 col-md-6">
+            <div className="footer-title pb-4">
+              <h5 className="text-light text-uppercase">QUICKLINKS</h5>
+            </div>
+            <div className="footer-second-text pb-3"></div>
+          </div>
+          <div className="col-xl-3 col-md-6">
+            <div className="footer-title pb-4">
+              <h5 className="text-light text-uppercase">QUICKLINKS</h5>
+            </div>
+            <div className="footer-second-text pb-3"></div>
+          </div>
         </div>
       </div>
     </div>
